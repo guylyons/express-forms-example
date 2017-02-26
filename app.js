c1de15a0a04f4f5e837aa75852171aecbe676e7d
Guy Lyons <guylyons2@gmail.com>
2017-02-26 07:39:24 -0500

    add in simple form example

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
+var querystring = require('querystring');
 
 var app = express();
 
 // Some global variables for our app to work with
 app.locals.title = "Express Simple Forms";
+app.locals.createdOn = function() {
+  return new Date();
+};
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // This is our template engine, current set to use Handlebars
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -15,6 +22,16 @@ app.set('view engine', 'handlebars');
 // is set to render our home template
 app.get('/', function (req, res) {
   res.render('home');
+});
+
+app.post('/test-form', function (req, res) {
+  res.json(req.body);
+});
+
+app.get('/addition', function (req, res) {
+  res.send(
+    querystring.parse(req.body)
+  );
 });
 
 app.listen(3000, function(err) {
